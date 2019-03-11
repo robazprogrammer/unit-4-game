92f4d0f535e87130759893fd1ac967263eaeb263
robazprogrammer <robazprogrammer@gmail.com>
2019-03-11 16:25:26 -0700

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,23 @@
-var random_result;
+var random_Number;
+var current_Number = 0;
 var lost = 0;
 var win = 0;
-var previous = 0;
 
-// Setters
-// Getters
-
-// $(".crystal").attr('class');
 
 var resetAndStart = function () {
 
 	$(".crystals").empty();
 
-	var images = [
-			'http://cdn.playbuzz.com/cdn/7a5d7935-6177-4be8-8b72-2a95ad2bcdfe/3b295cc9-7b5e-412f-8b1f-8547edd8e66b.jpg', 
-			'http://vignette3.wikia.nocookie.net/marvel-contestofchampions/images/1/1c/2-Star_Crystal.png/revision/latest?cb=20150825213642', 
-			'http://jonvilma.com/images/crystal-5.jpg', 
-			'https://static.turbosquid.com/Preview/2014/07/08__10_08_09/Crystals0010.jpgc22b2831-ae7a-4cb6-b4ac-612aa7f35ad7Original.jpg'];
+	var images = ["assets/images/crystal1.jpg","assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"];
 		
-	random_result = Math.floor(Math.random() * 69 ) + 30; 
+	random_Number = Math.floor(Math.random() * 102 ) + 19; 
 
 
-	$("#result").html('Random Result: ' + random_result);
+	$("#result").html('Random Number: ' + random_Number);
 
 	for(var i = 0; i < 4; i++){
 
-		var random = Math.floor(Math.random() * 11) + 1;
+		var random = Math.floor(Math.random() * 12) + 1;
 
 		var crystal = $("<div>");
 			crystal.attr({
@@ -43,7 +35,7 @@ var resetAndStart = function () {
 
 	}
 
-	$("#previous").html("Total Score: " + previous);
+	$("#current_Number").html("Total Score: " + current_Number);
 
 }
 
@@ -51,36 +43,35 @@ var resetAndStart = function () {
 resetAndStart();
 
 
-// Event Delegation
 $(document).on('click', ".crystal", function () {
 
 	var num = parseInt($(this).attr('data-random'));
 
-	previous += num;
+	current_Number += num;
 
 
-	$("#previous").html("Total score: " + previous);
+	$("#current_Number").html("Total score: " + current_Number);
 
-	console.log(previous);
+	console.log(current_Number);
 
-	if(previous > random_result){
+	if(current_Number > random_Number){
 
 		lost++;
 
-		$("#lost").html("You lost: " + lost);
+		$("#lost").html("Losses: " + lost);
 
-		previous = 0;
+		current_Number = 0;
 
 		resetAndStart();
 
 	} 
-	else if(previous === random_result){
+	else if(current_Number === random_Number){
 
 		win++;
 
-		$("#win").html("You win: " + win);
+		$("#win").html("Wins: " + win);
 
-		previous = 0;
+		current_Number = 0;
 
 		resetAndStart();
 
